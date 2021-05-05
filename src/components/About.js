e26070c58dc05e1e702ab06917c799ebbb79a52c
James.Sampedro <72242960+JamesSampedro@users.noreply.github.com>
2021-05-05 13:01:27 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <div>
-            <p className ='student'>James Paul B. Sampedro - BSIT32E2</p>
+            <p className ='student'>James Paul B. Sampedro  -  BSIT32E2</p>
         </div>
     )
 }
