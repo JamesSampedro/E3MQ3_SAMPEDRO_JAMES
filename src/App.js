c672d88b7ba8b1eeda56cb87dc7e76a378d303e6
James.Sampedro <72242960+Votum38git@users.noreply.github.com>
2021-05-05 12:54:47 +0800

    yes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import About from './components/About';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+
+
+
+function App() {
+  
+  return (
+    <Router>
+    <div className="container">
+      <Nav user = {'jamez.8833@gmail.com'}/>
+      <Switch>
+        <Route path = '/'exact component={Home}/>
+        <Route path = '/about'exact component={About}/>
+
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div>
+            <p className ='student'>James Paul B. Sampedro - BSIT32E2</p>
+        </div>
+    )
+}
+
+export default About
